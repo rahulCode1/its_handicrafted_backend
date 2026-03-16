60136569fd3d509ae2c176298eab850880de22c5
Rahul <pbrkumawat@gmail.com>
2026-03-16 15:46:40 +0530

    Char: Add cors

--- index.js
+++ index.js
@@ -14,7 +14,21 @@ const HttpError = require("./model/http-error")
 
 
 initializeDb()
-app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', "*")
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200)
+    }
+    next()
+});
+
 
 app.use(express.json())
 
