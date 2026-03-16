7b2011a4af149460eb85274bdbd475258b7cc647
Rahul <pbrkumawat@gmail.com>
2026-03-16 16:37:22 +0530

    Char: Add cors

--- index.js
+++ index.js
@@ -15,24 +15,12 @@ const HttpError = require("./model/http-error")
 
 initializeDb()
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', "*")
-//     res.setHeader(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//     );
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
 
-//     if (req.method === "OPTIONS") {
-//         return res.sendStatus(200)
-//     }
-//     next()
-// });
 
 
 app.use(cors({
     origin: ["http://localhost:3000", "https://itshandicrafted-frontend.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true
 }));
 
